49cec55641dfcc2e3f0963ff63f881e23d201aa0
Yuxin Yang <yangyuxin60@live.com>
2022-06-16 22:53:46 +0000

    add files

---
+++ batch_intfold.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+swiss = pd.read_csv('../uniprot-filtered-reviewed_yes.tab.gz', sep='\t')
+ec_swiss = swiss[swiss['EC number'].notnull()]
+
+for i in range(ec_swiss.shape[0]):
+    amino = ec_swiss.iloc[i]
+    entry = amino['Entry']
+    seq = amino['Sequence']
+    ec = amino['EC number']
+    os.system('python intfold.py --seq {} --entry {} --ec {}'.format(seq, entry, ec))
